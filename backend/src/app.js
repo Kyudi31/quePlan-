7850bffa1050612c41006f8a74535156bdf85bee
Kyudi31 <jmo8o7@hotmail.com>
2026-04-22 20:39:34 -0500

    initial base setup

---
+++ backend/src/app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+
+// Test route
+app.get("/", (req, res) => {
+  res.json({ message: "Backend funcionando 🚀" });
+});
+
+// Server
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
